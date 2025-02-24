645533daf5038070f7874f816b1ce9ce7e966032
Trajan0x <trajan0x@users.noreply.github.com>
2025-02-24 17:02:16 -0600

    enable branch level previews

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,10 +25,10 @@ org="${INPUT_ORG:-${FLY_ORG:-personal}}"
 image="$INPUT_IMAGE"
 config="${INPUT_CONFIG:-fly.toml}"
 
-if ! echo "$app" | grep "$PR_NUMBER"; then
-  echo "For safety, this action requires the app's name to contain the PR number."
-  exit 1
-fi
+#if ! echo "$app" | grep "$PR_NUMBER"; then
+#  echo "For safety, this action requires the app's name to contain the PR number."
+#  exit 1
+#fi
 
 # PR was closed - remove the Fly app if one exists and exit.
 if [ "$EVENT_TYPE" = "closed" ]; then
